8d25b1722fc748a3b9ba4115c94d2bc5dfea04cf
Nathan Breneman <63802479+nrbreneman@users.noreply.github.com>
2020-08-02 12:26:51 -0400

    Safe Auto Kata initial Commit
    Console App working

---
+++ SafeAutoKata/SafeAutoKata/SafeAutoKata/DAL.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SafeAutoKata
+{
+    class DAL
+    {
+        private string pathForFile;
+        public DAL(string path)
+        {
+            pathForFile = path;
+        }
+        public List<Driver> drivers = new List<Driver>();
+        public List<Trip> trips = new List<Trip>();
+
+
+        public void AccessFile()
+        {
+            try
+            {
+                using (StreamReader str = new StreamReader(pathForFile))
+                {
+                    while (!str.EndOfStream)
+                    {
+                        string line = str.ReadLine();
+                        if (line.Contains("Driver"))
+                        {
+                            List<string> tempData = new List<string>();
+                            tempData.Add(line);
+                            foreach (string name in tempData)
+                            {
+                                string subStr = name.Substring(7);
+                                Driver driver = new Driver();
+                                driver.Name = subStr;
+                                drivers.Add(driver);
+                            };//add new driver to system 
+                        }
+
+                        if (line.Contains("Trip"))
+                        {
+                            List<string> tempData = new List<string>();
+                            tempData.AddRange(line.Split(' '));
+                            Trip newTrip = new Trip();
+                            newTrip.DriverName = tempData[1];//et driver name from file
+                            newTrip.StartTime = Convert.ToDateTime(tempData[2]); //get start time from file 
+                            newTrip.EndTime = Convert.ToDateTime(tempData[3]); //get end time from file
+                            newTrip.Distance = Convert.ToDouble(tempData[4]); //get distance from file
+                            trips.Add(newTrip);
+
+
+                        }//add new trip to system
+
+                    }
+                }
+                foreach (Trip trip in trips)
+                {
+                    foreach (Driver driver in drivers)
+                    {
+                        if (driver.Name == trip.DriverName)
+                        {
+                            driver.trips.Add(trip);
+                            //add trip deatils to each driver 
+                        }
+
+
+                    }
+
+                };
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+
+    }
+}
---
+++ SafeAutoKata/SafeAutoKata/SafeAutoKata/Driver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafeAutoKata
+{
+    public class Driver
+    {
+        public string Name { get; set; }
+        public int MilesTraveled { get; set; }
+        public int AverageSpeed { get; set; }
+        public List<Trip> trips = new List<Trip>();
+
+        public void getAverageSpeed()
+        {
+            double distance = 0;
+            double timeTraveled = 0;
+            int speed = 0;
+            foreach (Trip trip in trips)
+            {
+                distance = trip.Distance + distance;
+                timeTraveled = trip.getTimeInHours(trip.StartTime, trip.EndTime) + timeTraveled;
+            }
+            speed = Convert.ToInt32(distance / timeTraveled);
+            if ((speed > 100) || (speed < 5))
+            {
+                AverageSpeed = 0;
+            }
+            else
+            {
+                AverageSpeed = speed;
+            }
+
+        }
+
+        public void getDistanceTraveled()
+        {
+            int totalDistance = 0;
+            foreach (Trip trip in trips)
+            {
+                totalDistance = Convert.ToInt32(totalDistance + trip.Distance);
+            }
+            MilesTraveled = totalDistance;
+        }
+    }
+}
+
