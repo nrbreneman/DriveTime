6e8bd6ac3944c41e675e904f14902ad9023ec78f
Nathan Breneman <nrbreneman@gmail.com>
2020-08-02 12:31:57 -0400

    Second Commit

---
+++ SafeAutoKata/SafeAutoKata/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SafeAutoKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C:/users/nrbre/SafeAutoKata.txt
+            Console.WriteLine("What is fully qualified name of the file that should be accessed?");
+            string pathForFile = Console.ReadLine();
+            DAL data = new DAL(pathForFile);
+            data.AccessFile();
+            Console.WriteLine();
+            Console.WriteLine("The following are the results of the file that was input:");
+            foreach (Driver driver in data.drivers)
+            {
+
+                driver.getDistanceTraveled();
+                if (driver.MilesTraveled > 0)
+                {
+                    driver.getAverageSpeed();
+
+                    Console.WriteLine(driver.Name + ": " + driver.MilesTraveled + " miles @ " + driver.AverageSpeed + "mph");
+                }
+                else
+                {
+                    Console.WriteLine(driver.Name + ": 0 miles");
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SafeAutoKata/SafeAutoKata/Trip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafeAutoKata
+{
+    public class Trip
+    {
+        public string DriverName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double Distance { get; set; }
+
+        public double getTimeInHours (DateTime startTime, DateTime endTime)
+        {
+            double totalTime = 0;
+            TimeSpan ts = endTime - startTime;
+            totalTime = Convert.ToDouble(ts.TotalHours);
+            return totalTime;
+        }
+    }
+}
